7c505224d5564378f9b90c4a2756cfd80c53b6ce
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-12-15 20:12:11 -0300

    Upload to github

---
+++ api/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# import user and group standard models
+from django.contrib.auth.models import User, Group
+
+# import restframework
+from rest_framework import viewsets
+from rest_framework import permissions
+from api.serializers import UserSerializer, GroupSerializer
+
+# Create your views here.
+def front(request):
+    context = { }
+    return render(request, "index.html", context)
+
+
+# Models Views
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated] #Specific permission
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    # permission_classes = [permissions.IsAuthenticated] #Specific permission 
+
